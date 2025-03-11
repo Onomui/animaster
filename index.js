5a39246c69eb18b53b8a0756626738ecbe0bfa0f
Shutcing <maksim19111@gmail.com>
2025-03-11 17:49:49 +0500

    -

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
 addListeners();
 
 function addListeners() {
-    document.getElementById('fadeInPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeInBlock');
-            animaster().fadeIn(block, 5000);
-        });
+  document.getElementById("fadeInPlay").addEventListener("click", function () {
+    const block = document.getElementById("fadeInBlock");
+    animaster().fadeIn(block, 5000);
+  });
 
-    document.getElementById('movePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('moveBlock');
-            animaster().move(block, 1000, {x: 100, y: 10});
-        });
+  document.getElementById("movePlay").addEventListener("click", function () {
+    const block = document.getElementById("moveBlock");
+    animaster().move(block, 1000, { x: 100, y: 10 });
+  });
 
-    document.getElementById('scalePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('scaleBlock');
-            animaster().scale(block, 1000, 1.25);
-        });
+  document.getElementById("scalePlay").addEventListener("click", function () {
+    const block = document.getElementById("scaleBlock");
+    animaster().scale(block, 1000, 1.25);
+  });
 
-    document.getElementById('fadeOutPlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('fadeOutBlock');
-            animaster().fadeOut(block, 5000);
-        });
+  document.getElementById("fadeOutPlay").addEventListener("click", function () {
+    const block = document.getElementById("fadeOutBlock");
+    animaster().fadeOut(block, 5000);
+  });
 
-    document.getElementById('moveAndHidePlay')
-        .addEventListener('click', function () {
-            const block = document.getElementById('moveAndHideBlock');
-            animaster().moveAndHide(block, 1000)
-        });
+  document
+    .getElementById("moveAndHidePlay")
+    .addEventListener("click", function () {
+      const block = document.getElementById("moveAndHideBlock");
+      animaster().moveAndHide(block, 1000);
+    });
 
-    document.getElementById("showAndHidePlay")
-        .addEventListener("click", function () {
-            const block = document.getElementById("showAndHideBlock");
-            animaster().showAndHide(block, 1000, 1.25);
-        });
+  document
+    .getElementById("showAndHidePlay")
+    .addEventListener("click", function () {
+      const block = document.getElementById("showAndHideBlock");
+      animaster().showAndHide(block, 1000, 1.25);
+    });
 }
 
 function animaster() {
@@ -86,9 +84,9 @@ function animaster() {
     },
 
     moveAndHide(element, duration) {
-        this.move(element, duration * 0.4, {x: 100, y:20});
-        setTimeout(this.fadeOut, duration * 0.4, element, duration * 0.6);
-    }
+      this.move(element, duration * 0.4, { x: 100, y: 20 });
+      setTimeout(this.fadeOut, duration * 0.4, element, duration * 0.6);
+    },
   };
 }
 
